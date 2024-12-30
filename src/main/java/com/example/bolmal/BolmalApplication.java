7d71e4dbb4480a3cf0b604cc5ea5e1edeea9ab02
gdbs1107 <gdbs1107@naver.com>
2024-12-30 11:54:32 +0900

    [init] 초기설정

---
+++ src/main/java/com/example/bolmal/BolmalApplication.java
@@ -0,0 +1,13 @@
+package com.example.bolmal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolmalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BolmalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bolmal/BolmalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bolmal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BolmalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
