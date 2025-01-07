4b572bade52cfa78942970c18a84f39d88080b34
gdbs1107 <gdbs1107@naver.com>
2025-01-07 18:13:42 +0900

    [feat] 로그인 기능 마이그레이션

---
+++ src/main/java/com/example/bolmal/config/repository/MemberRepository.java
@@ -0,0 +1,12 @@
+package com.example.bolmal.config.repository;
+
+import com.example.bolmal.config.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByUsername(String username);
+
+}
