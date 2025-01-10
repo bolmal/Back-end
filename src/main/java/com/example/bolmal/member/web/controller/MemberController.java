b97c4742a0a8fa78fd979b2f1542b284616d6782
gdbs1107 <gdbs1107@naver.com>
2025-01-11 00:51:43 +0900

    [feat] username 테스트 작성

--- src/main/java/com/example/bolmal/member/web/controller/MemberController.java
+++ src/main/java/com/example/bolmal/member/web/controller/MemberController.java
@@ -5,6 +5,7 @@ import com.example.bolmal.member.web.port.MemberService;
 import com.example.bolmal.member.web.dto.MemberJoinDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -21,7 +22,7 @@ public class MemberController {
     // 기획서대로 수정 필요함 FIXME
     @Operation(summary = "회원가입 API")
     @PostMapping("/join")
-    public ApiResponse<MemberJoinDTO.MemberJoinResponseDTO> join(@RequestBody MemberJoinDTO.MemberJoinRequestDTO request) {
+    public ApiResponse<MemberJoinDTO.MemberJoinResponseDTO> join(@Valid @RequestBody MemberJoinDTO.MemberJoinRequestDTO request) {
 
         MemberJoinDTO.MemberJoinResponseDTO response = memberService.joinMember(request);
 
