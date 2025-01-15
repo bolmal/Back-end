bd8c91c513367e457f67af02f52bd646eb97abf9
gdbs1107 <gdbs1107@naver.com>
2025-01-15 21:20:14 +0900

    [feat] 이메일 인증 엔드포인트 수정

--- src/main/java/com/example/bolmal/mail/web/controller/EmailController.java
+++ src/main/java/com/example/bolmal/mail/web/controller/EmailController.java
@@ -4,6 +4,8 @@ import com.example.bolmal.common.apiPayLoad.ApiResponse;
 import com.example.bolmal.mail.service.MailServiceImpl;
 import com.example.bolmal.mail.web.dto.EmailRequestDto;
 import com.example.bolmal.mail.web.port.MailService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -12,12 +14,14 @@ import java.security.NoSuchAlgorithmException;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/redis_email")
+@RequestMapping("/email")
+@Tag(name = "이메일 인증 API")
 public class EmailController {
 
     private final MailService emailService;
 
-    @GetMapping("/{email_addr}/authcode")
+    @GetMapping("/{email_addr}")
+    @Operation(summary = "인증 이메일 전송 API")
     public ApiResponse<String> sendEmailPath(@PathVariable String email_addr) throws MessagingException {
 
         emailService.sendEmail(email_addr);
@@ -25,7 +29,8 @@ public class EmailController {
         return ApiResponse.onSuccess("이메일이 전송되었습니다");
     }
 
-    @PostMapping("/{email_addr}/authcode")
+    @PostMapping("/{email_addr}")
+    @Operation(summary = "인증 번호 검증 API")
     public ApiResponse<String> sendEmailAndCode(@PathVariable String email_addr,
                                                    @RequestBody EmailRequestDto dto) throws NoSuchAlgorithmException {
 
