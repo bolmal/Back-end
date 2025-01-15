f143eea89a88831e819870fcf153090ba99e71ab
gdbs1107 <gdbs1107@naver.com>
2025-01-15 20:54:33 +0900

    [feat] mailservice 추상화

--- src/main/java/com/example/bolmal/mail/service/MailServiceImpl.java
+++ src/main/java/com/example/bolmal/mail/service/MailServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.bolmal.mail.service;
 
 import com.example.bolmal.mail.util.RedisUtil;
+import com.example.bolmal.mail.web.port.MailService;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
@@ -23,13 +24,49 @@ import java.util.Random;
 @Service
 @RequiredArgsConstructor
 @Profile("local")
-public class RedisMailService {
+public class MailServiceImpl implements MailService {
 
     private final JavaMailSender mailSender;
     private final RedisUtil redisUtil;
 
     @Value("${management.mail.username}")
     private String configEmail;
+
+    /**
+     *
+     * 제작한 이메일을 전송합니다
+     *
+     * 이때 redis에 이미 해당 이메일로 인증번호가 전송된 이력이 있다면,
+     * 삭제하고 대체합니다
+     *
+     * */
+    @Override
+    public void sendEmail(String toEmail) throws MessagingException {
+        if (redisUtil.existData(toEmail)) {
+            redisUtil.deleteData(toEmail);
+        }
+
+        MimeMessage emailForm = createEmailForm(toEmail);
+
+        mailSender.send(emailForm);
+    }
+
+    /**
+     *
+     * 이메일을 검증합니다
+     * email을 키값으로 갖는 데이터를 조회하여 일치하는지 조사합니다
+     *
+     * */
+    @Override
+    public Boolean verifyEmailCode(String email, String code) {
+        String codeFoundByEmail = redisUtil.getData(email);
+        if (codeFoundByEmail == null) {
+            return false;
+        }
+        return codeFoundByEmail.equals(code);
+    }
+
+
 
 
     /**
@@ -86,49 +123,13 @@ public class RedisMailService {
 
     /**
      *
-     * 제작한 이메일을 전송합니다
-     *
-     * 이때 redis에 이미 해당 이메일로 인증번호가 전송된 이력이 있다면,
-     * 삭제하고 대체합니다
-     *
-     * */
-    public void sendEmail(String toEmail) throws MessagingException {
-        if (redisUtil.existData(toEmail)) {
-            redisUtil.deleteData(toEmail);
-        }
-
-        MimeMessage emailForm = createEmailForm(toEmail);
-
-        mailSender.send(emailForm);
-    }
-
-    /**
-     *
-     * 이메일을 검증합니다
-     * email을 키값으로 갖는 데이터를 조회하여 일치하는지 조사합니다
-     *
-     * */
-    public Boolean verifyEmailCode(String email, String code) {
-        String codeFoundByEmail = redisUtil.getData(email);
-        if (codeFoundByEmail == null) {
-            return false;
-        }
-        return codeFoundByEmail.equals(code);
-    }
-
-
-
-
-
-
-    /**
-     *
      * 해당하는 멤버ID를 제작하는 메서드입니다
      *
      * 추후 멤버식별자를 구성할 때 사용 할 수 있겠지만,
      * 당장은 auto_increasement로 사용하겠습니다
      *
      * */
+    @Override
     public String makeMemberId(String email) throws NoSuchAlgorithmException {
         MessageDigest md = MessageDigest.getInstance("SHA-256");
         md.update(email.getBytes());
--- src/main/java/com/example/bolmal/mail/web/controller/EmailController.java
+++ src/main/java/com/example/bolmal/mail/web/controller/EmailController.java
@@ -1,11 +1,11 @@
 package com.example.bolmal.mail.web.controller;
 
 import com.example.bolmal.common.apiPayLoad.ApiResponse;
-import com.example.bolmal.mail.service.RedisMailService;
+import com.example.bolmal.mail.service.MailServiceImpl;
 import com.example.bolmal.mail.web.dto.EmailRequestDto;
+import com.example.bolmal.mail.web.port.MailService;
 import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.NoSuchAlgorithmException;
@@ -13,9 +13,9 @@ import java.security.NoSuchAlgorithmException;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/redis_email")
-public class RedisEmailController {
+public class EmailController {
 
-    private final RedisMailService emailService;
+    private final MailService emailService;
 
     @GetMapping("/{email_addr}/authcode")
     public ApiResponse<String> sendEmailPath(@PathVariable String email_addr) throws MessagingException {
