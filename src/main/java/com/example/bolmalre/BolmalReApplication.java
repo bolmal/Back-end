256283402aa347a6b7c909d800a41a1181716de0
gdbs1107 <gdbs1107@naver.com>
2025-01-25 20:31:16 +0900

    [init] 프로젝트 초기 설정

---
+++ src/main/java/com/example/bolmalre/BolmalReApplication.java
@@ -0,0 +1,13 @@
+package com.example.bolmalre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolmalReApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BolmalReApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bolmalre/BolmalReApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bolmalre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BolmalReApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
