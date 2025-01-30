502b409f606a06006dcfeb4599c63e8f00e64dc7
gdbs1107 <gdbs1107@naver.com>
2025-01-30 22:31:23 +0900

    [chore] 시큐리티 코드 수정

--- src/main/java/com/example/bolmalre/config/SecurityConfig.java
+++ src/main/java/com/example/bolmalre/config/SecurityConfig.java
@@ -16,6 +16,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -85,10 +86,10 @@ public class SecurityConfig {
 
 
 
-        http.csrf((auth)->auth.disable());
+        http.csrf(AbstractHttpConfigurer::disable);
 
-        http.formLogin((auth)->auth.disable());
-        http.httpBasic((auth)->auth.disable());
+        http.formLogin(AbstractHttpConfigurer::disable);
+        http.httpBasic(AbstractHttpConfigurer::disable);
 
 
         http.authorizeHttpRequests((auth)->auth
