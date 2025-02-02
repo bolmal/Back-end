3dea4265a3d7255c62961f34537dac3f4c235ba7
gdbs1107 <gdbs1107@naver.com>
2025-02-02 20:13:52 +0900

    [fix] 주석 삭제

--- src/main/java/com/example/bolmalre/bookmark/web/controller/BookmarkController.java
+++ src/main/java/com/example/bolmalre/bookmark/web/controller/BookmarkController.java
@@ -32,17 +32,7 @@ public class BookmarkController {
         return ApiResponse.onSuccess("성공적으로 구매가 완료되었습니다");
     }
 
-    /**
-     찜 등록하기
 
-     아티스트를 찜하는 기능입니다
-     찜하면 회원의 찜 쿠폰이 하나 줄어들고
-
-     아티스트의 공연이 업로드 될 때마다 그 사람한테 메일을 보내줍니다
-
-     찜 설정이 완료되면 찜 설정확인 메일을 전송합니다
-     알림받을 아티스트 정보
-     * */
     @PostMapping("")
     @Operation(summary = "아티스트 찜 등록 API")
     public ApiResponse<BookmarkRegisterDTO.BookmarkRegisterResponseDTO> registerBookmark(@AuthenticationPrincipal UserDetails userDetails,
@@ -54,23 +44,6 @@ public class BookmarkController {
     }
 
 
-
-    /**
-     *
-     * 등록 확인창 조회 API
-     *
-     * 이것도 필요한가
-     *
-     * */
-
-
-
-    /**
-     * 찜한 아티스트 조회
-     *
-     * 프로필 사진, 아티스트 이름, 장르
-     *
-     * */
     @GetMapping("")
     @Operation(summary = "찜한 아티스트 조회 API")
     public ApiResponse<List<BookmarkGetArtistDTO.BookmarkGetArtistResponseDTO>> getArtist(@AuthenticationPrincipal UserDetails userDetails) {
