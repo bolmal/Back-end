7e5ca530244dcdaea8e3cff13875e904631993a2
gdbs1107 <gdbs1107@naver.com>
2025-02-05 20:13:35 +0900

    [chore] SSR 동적으로 redirect 할 수 있도록 수정

--- src/main/java/com/example/bolmalre/member/web/controller/MemberOAuthSSRController.java
+++ src/main/java/com/example/bolmalre/member/web/controller/MemberOAuthSSRController.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,14 +17,30 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/oauth/ssr")
-@Tag(name = "소셜로그인 API")
+@Tag(name = "소셜로그인 SSR")
 public class MemberOAuthSSRController {
 
     private final OAuthServiceImpl authService;
 
+    @Value("${spring.security.oauth2.client.registration.kakao.client-id}")
+    private String kakaoClientId;
+
+    @Value("${spring.security.oauth2.client.registration.kakao.redirect-uri}")
+    private String kakaoRedirectUri;
+
+    @Value("${spring.security.oauth2.client.registration.naver.client-id}")
+    private String naverClientId;
+
+    @Value("${spring.security.oauth2.client.registration.naver.redirect-uri}")
+    private String naverRedirectUri;
+
     @GetMapping("/login")
-    public String loginPage() {
-        return "oauth/login";
+    public String loginPage(Model model) {
+        model.addAttribute("kakaoClientId", kakaoClientId);
+        model.addAttribute("kakaoRedirectUri", kakaoRedirectUri);
+        model.addAttribute("naverClientId", naverClientId);
+        model.addAttribute("naverRedirectUri", naverRedirectUri);
+        return "oauth/login"; // Thymeleaf 템플릿 경로
     }
 
     @GetMapping("/kakao/callback")
