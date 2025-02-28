6c17d0a215b22fb41b199edcca25e4bd7d6cbffd
Hwigun46 <hy010827@naver.com>
2025-03-01 04:02:39 +0900

    [feat] 필요 쿼리 추가

--- src/main/java/com/example/bolmalre/concert/infrastructure/ConcertTicketRoundRepository.java
+++ src/main/java/com/example/bolmalre/concert/infrastructure/ConcertTicketRoundRepository.java
@@ -2,7 +2,9 @@ package com.example.bolmalre.concert.infrastructure;
 
 import com.example.bolmalre.concert.domain.Concert;
 import com.example.bolmalre.concert.domain.ConcertTicketRound;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -14,4 +16,12 @@ public interface ConcertTicketRoundRepository extends JpaRepository<ConcertTicke
     Optional<ConcertTicketRound> findFirstByConcertAndTicketOpenDateAfterOrderByTicketOpenDateAsc(Concert concert, LocalDateTime ticketOpenDate);
 
     List<ConcertTicketRound> findAllByConcert(Concert concert);
+
+    @Query("""
+    SELECT tr FROM ConcertTicketRound tr
+    WHERE tr.ticketOpenDate >= :now
+    ORDER BY tr.ticketOpenDate ASC
+    LIMIT 1
+""")
+    Optional<ConcertTicketRound> findNearestConcertTicket(@Param("now") LocalDateTime now);
 }
