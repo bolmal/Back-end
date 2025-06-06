ee479d0538e6c328235b6afb8d9316d4cf865e2d
gdbs1107 <gdbs1107@naver.com>
2025-06-06 17:02:49 +0900

    feat: 이미지 수정

--- src/main/java/com/example/bolmalre/global/auth/filter/LoginFilter.java
+++ src/main/java/com/example/bolmalre/global/auth/filter/LoginFilter.java
@@ -107,7 +107,7 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
         result.put("isLogin", byUsername.isLogin());
         result.put("alarmCount", byUsername.getAlarmAccount());
         result.put("upComming", "test");
-        result.put("imagePath", "https://bolmal-bucket.s3.ap-northeast-2.amazonaws.com/aa.png");
+        result.put("imagePath", null);
         result.put("name", byUsername.getName());
         result.put("bookmarkCount", byUsername.getBookmarkAccount());
         result.put("isSubscribe", byUsername.getStatus());
